7795e955a246d7a735b4106e1f5a7a1754b879bf
Megan Healey <healeymt@vcu.edu>
2025-09-03 15:46:44 -0400

    add script.js

--- script.js
+++ script.js
@@ -4,5 +4,9 @@ sounds.forEach((sound) => {
     const btn = document.createElement('button');
     btn.classList.add('btn');
     btn.innerText = sound;
+    btn.addEventListener('click', () => {
+        document.getElementById(sound).play();
+    });
     document.getElementById('buttons').appendChild(btn);
 });
+
